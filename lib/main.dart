5531ecc3e0d12ce39d4d098ff3dcf34a44921573
Muhammad Ahmer Khan <70189037+mahmerkhan@users.noreply.github.com>
2025-03-17 23:06:37 +0500

    feat : updated protfolio with adding images support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // appBar: AppBar(title: const Text('My Portfolio')),
-        body: const Center(child: Text('Hello Ahmer')),
+        body: Stack(
+          children: [
+            /// Background Image
+            Positioned.fill(
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/california.jpeg"),
+                    fit: BoxFit.cover, // Makes image responsive
+                  ),
+                ),
+              ),
+            ),
+
+            /// Text Content (Right-Top Aligned)
+            Positioned(
+              top: 20, 
+              right: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    "Muhammad Ahmer Khan",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  
+                  const SizedBox(height: 4),
+                  const Text(
+                    "B.S., University of Karachi, 2022",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  /// GitHub Logo with Link
+                  GestureDetector(
+                    onTap: () => launchUrl(Uri.parse("https://github.com/mahmerkhan")),
+                    child: Image.asset(
+                      "assets/images/github-mark.png",
+                      width: 30,
+                      height: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
