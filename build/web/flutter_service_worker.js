e848852c27277c1992177ac4a7bfac480e476255
Muhammad Ahmer Khan <70189037+mahmerkhan@users.noreply.github.com>
2025-03-18 23:31:22 +0500

    updated

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "df361fb668ff372159672837d02ac27b",
+const RESOURCES = {"flutter_bootstrap.js": "fc47615f3cabf3115a354968a075d300",
 "version.json": "009c9e65172e010890f7f65fde438006",
 "index.html": "80b7f3c2a7bb29c643595523dbc0fc2e",
 "/": "80b7f3c2a7bb29c643595523dbc0fc2e",
